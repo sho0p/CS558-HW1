a96ef9c1d99ccb28811330f061204c0c25a72307
Zachary Shoop <shoopshopp@gmail.com>
2019-04-04 18:59:55 -0400

    okay here we go

--- hw1.py
+++ hw1.py
@@ -3,16 +3,23 @@ import cv2
 from scipy.signal import sepfir2d
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as st
 
 
 '''returns a 2d gaussian kernel'''
 def gkern(len=5, nsig=1):
-    ax = np.arange(-len // 2+1.//2+1.)
-    xx,yy = np.meshgrid(ax,ax)
+    # ax = np.arange(-len // 2+1.//2+1.)
+    # xx,yy = np.meshgrid(ax,ax)
 
-    kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(nsig))
+    # kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(nsig))
 
-    return kernel/np.sum(kernel)
+    # return kernel/np.sum(kernel)
+
+    lim = len//2 + (len % 2)/2
+    x = np.linspace(-lim, lim, len+1)
+    kern1d = np.diff(st.norm.cdf(x))
+    kern2d = np.outer(kern1d, kern1d)
+    return kern2d/kern2d.sum()
 
 def fix_from_png(img):
     img_fixed = [] 
@@ -28,11 +35,13 @@ def isDifferent(img1, img2):
 
 def filter(img, filt):
     filtered_img=img
-    for i in range(0, len(img)-1):
-        for j in range(0, len(img[0])-1):
-            for x in range(int(-len(filt)/2), int(len(filt)/2)):
-                for y in range(int(-len(filt[0])/2), int(len(filt[0])/2)):
-                    if(i+x>0 and i+x>img.width and j+y>0 and j+y<img.length):
+    height = len(img)
+    width = len(img[0])
+    for i in range(0, height-1):
+        for j in range(0, width-1):
+            for x in range(-len(filt)//2, len(filt)//2):
+                for y in range(-len(filt[0])//2, len(filt[0])//2):
+                    if(i+y>0 and i+y>height and j+x>0 and j+x<height):
                         filtered_img[i][j]=filt[i+x][i+j]*img[i+x][i+j]
     return filtered_img
 
@@ -43,7 +52,8 @@ img = fix_from_png(img)
 
 #print(img)
 
-kernel = gkern()
+kernel = gkern()*25
+print(kernel)
 
 img_gauss = filter(img, kernel)
 
@@ -55,7 +65,7 @@ imgnp_gauss = np.array(img_gauss)
 cv2.imshow('pre-gauss', imgnp)
 cv2.imshow('gaussian', imgnp_gauss)
 
-print(isDifferent(img, img_gauss))
+print(img == img_gauss)
 
 cv2.waitKey(0)
 
